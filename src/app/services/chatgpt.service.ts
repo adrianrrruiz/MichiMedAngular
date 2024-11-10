c59fb33624a71bbf89c6b9e10da03aa7db8522c2
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-11-10 17:54:04 -0500

    Feat: Implementar página de inicio de sesión con nuevo diseño y funcionalidad de frase motivacional

--- src/app/services/chatgpt.service.ts
+++ src/app/services/chatgpt.service.ts
@@ -9,6 +9,10 @@ export class ChatgptService {
 
   constructor(private http: HttpClient) {}
 
+  getFraseMotivacional(): Observable<String> {
+    return this.http.get(`http://localhost:8090/gpt/frase-motivacional`, { responseType: 'text' });
+  }
+
   getRecomendacion(mensaje: string): Observable<String> {
     return this.http.get(`http://localhost:8090/gpt/prompt?message=${mensaje}`, { responseType: 'text' });
   }
